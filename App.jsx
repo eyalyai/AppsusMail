642aa8615ed80c5553fd24180c2640101fbdbbf9
Aylam BarOn <Aylambo@gmail.com>
2021-05-04 11:06:17 +0300

    working?

--- App.jsx
+++ App.jsx
@@ -1,19 +1,11 @@
 const Router = ReactRouterDOM.HashRouter
 const { Route, Switch, Link } = ReactRouterDOM
 
-<<<<<<< HEAD
-import { BookShopApp } from './apps/email/pages/BookShopApp.jsx'
-import { BookDetails } from './pages/BookDetails.jsx'
-import { AboutUs } from './pages/AboutUs.jsx'
-import { AppHeader } from './cmps/AppHeader.jsx'
-import { MailApp } from './apps/email/MailApp.jsx'
-=======
-import {BookShopApp} from './apps/email/pages/BookShopApp.jsx'
+import {MailApp} from './apps/email/MailApp.jsx'
 import {MissKeep} from './apps/keep/MissKeep.jsx'
 import {AboutUs} from './pages/AboutUs.jsx'
 import {AppHeader} from './cmps/AppHeader.jsx'
 import {BookEdit} from './pages/BookEdit.jsx'
->>>>>>> 8537dbb2bb5a20380859a625d6a638a34c1ab5b7
 
 function Home() {
     return <section className="home-page">
@@ -30,19 +22,12 @@ export function App() {
             </header>
             <main>
                 <Switch>
-<<<<<<< HEAD
+                    {/* <Route component={BookEdit} path="/book/edit/:bookId?"/> */}
                     <Route component={ MailApp } path="/mail" />
-                    <Route component={ BookDetails } path="/book/:bookId" />
-                    <Route component={ BookShopApp } path="/book" />
-                    <Route component={ AboutUs } path="/about" />
-                    <Route component={ Home } path="/" />
-=======
-                    <Route component={BookEdit} path="/book/edit/:bookId?"/>
                     <Route component={MissKeep} path="/keep"/>
-                    <Route component={BookShopApp} path="/book"/>
+                    {/* <Route component={BookShopApp} path="/book"/> */}
                     <Route component={AboutUs} path="/about"/>
                     <Route component={Home} path="/"/>
->>>>>>> 8537dbb2bb5a20380859a625d6a638a34c1ab5b7
                 </Switch>
             </main>
             <footer>
