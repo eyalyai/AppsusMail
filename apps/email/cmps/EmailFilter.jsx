1eee7130ed7e571b114030055518bc3a4a56d2c3
eyal258@gmail.com <eyal258@gmail.com>
2021-05-08 17:54:36 +0300

    sat 6:00pm eyal

--- apps/email/cmps/EmailFilter.jsx
+++ apps/email/cmps/EmailFilter.jsx
@@ -1,9 +1,11 @@
 import { EmailStatus } from './EmailStatus.jsx'
 export class EmailFilter extends React.Component {
     state = {
+        searchBy: 'sender',
         filterBy: {
             IsStared: false,
             readStatus: '',
+            // all: '',
             sender: '',
             subject: '',
             body: ''
@@ -11,7 +13,8 @@ export class EmailFilter extends React.Component {
     }
 
     handleChange = (ev) => {
-        const field = ev.target.name
+        console.log(ev.target.name);
+        const field = this.state.searchBy
         const value = ev.target.type === 'number' ? +ev.target.value : ev.target.value
         this.setState({ filterBy: { ...this.state.filterBy, [field]: value } }, () => {
             this.props.onSetFilter(this.state.filterBy)
@@ -23,35 +26,33 @@ export class EmailFilter extends React.Component {
         this.props.onSetFilter(this.state.filterBy)
     }
 
+    onSelect = (ev) => {
+        ev.preventDefault()
+        const searchBy = ev.target.value
+        this.setState({ searchBy })
+    }
+
+
 
     render() {
-        // console.log(this.state.filterBy);
-        // const { text } = this.state.filterBy
-        const { sender, subject, body } = this.state.filterBy
+        const { searchBy, filterBy } = this.state
 
         return (
-            <section className="email-search flex align-center space-between">
+            <section className="email-filter flex align-center">
                 <form className="email-filterby flex wrap justify-center" onSubmit={ this.onFilter }>
 
-                    <input type="text" id="sender" name="sender" placeholder="By Sender" value={ sender }
+                    <input className="email-filter-ipt" type="text" placeholder={ 'Search by ' + searchBy } value={ filterBy[searchBy] }
                         onChange={ this.handleChange } />
-
-                    <input type="text" id="subject" name="subject" placeholder="By Subject" value={ subject }
-                        onChange={ this.handleChange } />
-
-                    <input type="text" id="body" name="body" value={ body } placeholder="By Body" onChange={ this.handleChange } />
+                    <select onChange={ this.onSelect }>
+                        {/* <option name="all">all</option> */ }
+                        <option name="sender">sender</option>
+                        <option name="subject">subject</option>
+                        <option name="body">body</option>
+                    </select>
+                    {/* <button type="submit">Go</button>     */ }
                 </form>
                 <EmailStatus emails={ this.props.emails } />
             </section>
-            // <div>
-            //     <select className="mail-filter-by" name="readStatus" onChange={ this.handleChange }>
-            //         <option value="all">All</option>
-            //         <option value="read">Read</option>
-            //         <option value="unread">Unread</option>
-            //     </select>
-            //     <input className="email-search" type="text" placeholder="Search mail" value={ text }
-            //         onChange={ this.handleChange } />
-            // </div>
         )
     }
 }
